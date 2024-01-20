c9dc1668ec79301815c20b31be527d20d8b35be4
t1oluwani <ti.favour@gmail.com>
2024-01-19 22:52:31 -0700

    Add story form

--- frontend/src/components/AddStory/AddStory.js
+++ frontend/src/components/AddStory/AddStory.js
@@ -1,8 +1,19 @@
-
+// CONTENTS OF THIS SHOULD BE ON SEPERATE PAGE
 
 function AddStory() {
     return (
-        <div></div>
+        <form className='add-form'>
+            <div className='form-control'>
+                <label>User Story</label>
+                <input type="text" placeholder="As a [who], I want to [what], so that [why]."/>
+            </div>
+            <div className='form-control'>
+                <label>Difficulty Level</label>
+                <input type="number" placeholder="0"/>
+            </div>
+
+            <p><input type="submit" value="Save" /> <input type="submit" value="Cancel" /></p>
+        </form>
     )
 }
 
