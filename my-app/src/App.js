c3c81c90611d3ec90908c21a83c1205ff0e4dd4f
Sumaiya Salsabil <117234514+sumaiyasalsabil@users.noreply.github.com>
2024-01-20 11:06:42 -0700

    progress bar w hardcoded values

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import ProgressBar from './components/progressbar';
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+
+    return () => {
+      
+    }
+  }, [progress]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProgressBar value={progress}/>
     </div>
   );
 }
