bf887d49c4df0f90d58bec6de24c4d704cb5e7f4
t1oluwani <ti.favour@gmail.com>
2024-01-20 17:40:49 -0700

    edit and delete btn

--- frontend/src/components/Story/story.js
+++ frontend/src/components/Story/story.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function Story ( {story_id, story_name, points, checkbox, onToggleCheckbox, onDelete} ){
+
+    const [isHovered, setIsHovered] = useState(false);
 
     const styles = {
         outerShell: {
@@ -22,6 +26,21 @@ function Story ( {story_id, story_name, points, checkbox, onToggleCheckbox, onDe
         checkbox: {
             marginLeft: "auto",
             marginRight: "5vw"
+        },
+        button: {
+            padding: "10px 15px",
+            border: "none",
+            borderRadius: "5px",
+            // background: "linear-gradient(180deg, rgba(255, 174, 105, 1) 0%, rgba(255, 173, 102, 1) 32%, rgba(255, 132, 25, 1) 100%)",
+            color: "white",
+            cursor: "pointer",
+            fontSize: "16px",
+            fontWeight: "bold",
+            margin: "50px 0px 50px 5px",
+            transition: "background 0.3s", 
+            background: isHovered
+                ? "linear-gradient(180deg, rgba(255, 132, 25, 1) 0%, rgba(255, 173, 102, 1) 68%, rgba(255, 174, 105, 1) 100%)"
+                : "linear-gradient(180deg, rgba(255, 174, 105, 1) 0%, rgba(255, 173, 102, 1) 32%, rgba(255, 132, 25, 1) 100%)",
         }
     };
 
@@ -37,7 +56,12 @@ function Story ( {story_id, story_name, points, checkbox, onToggleCheckbox, onDe
             <div style={styles.clickables}>
                 <input type="checkbox" checked={checkbox}
                  style={styles.checkbox} onChange={() => onToggleCheckbox(story_id, checkbox)}/>
-                <button>Edit</button> <button onClick={() => onDelete(story_id)}>Delete</button>                
+                <button style={styles.button} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                    Edit
+                </button>
+                <button style={styles.button} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} onClick={() => onDelete(story_id) }>
+                    Delete
+                </button>                
             </div>
         </div>
     )
