ffd274980dcf880c74daee53f635040e1c72f127
t1oluwani <ti.favour@gmail.com>
2024-01-23 01:58:00 -0700

    edit/delete btns, check+delete effect

--- frontend/src/components/Story/story.js
+++ frontend/src/components/Story/story.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 function Story ( {story_id, story_name, points, checkbox, onToggleCheckbox, onDelete} ){
 
-    const [isHovered, setIsHovered] = useState(false);
+    const [check, setChecked] = useState(false);
+    const [editIsHovered, setEditIsHovered] = useState(false);
+    const [deleteIsHovered, setDeleteIsHovered] = useState(false);
+
 
     const styles = {
         outerShell: {
@@ -10,9 +13,15 @@ function Story ( {story_id, story_name, points, checkbox, onToggleCheckbox, onDe
             flexDirection: "center",
             width: "60%",
             margin: "5vh auto 0 auto",
-            border: "4px solid",
+            border: deleteIsHovered 
+                ? "4px solid red" 
+                : (check 
+                    ? "4px solid green" 
+                    : "4px solid"),
             borderRadius: "25px",
             justifyContent: "left",
+            background: "rgba(255, 213, 128, 0.4)",
+
         },
         storyText: {
             textAlign: "left",
@@ -24,23 +33,40 @@ function Story ( {story_id, story_name, points, checkbox, onToggleCheckbox, onDe
             display: "flex",
         },
         checkbox: {
-            marginLeft: "auto",
-            marginRight: "5vw"
+            margin: "60px 0px 0px 0px",
+            marginRight: "25px",
+            width: "25px",  
+            height: "25px", 
+            borderRadius: "5px", 
+            backgroundColor: "green",
         },
-        button: {
+        edit: {
             padding: "10px 15px",
             border: "none",
             borderRadius: "5px",
-            // background: "linear-gradient(180deg, rgba(255, 174, 105, 1) 0%, rgba(255, 173, 102, 1) 32%, rgba(255, 132, 25, 1) 100%)",
             color: "white",
             cursor: "pointer",
             fontSize: "16px",
             fontWeight: "bold",
             margin: "50px 0px 50px 5px",
-            transition: "background 0.3s", 
-            background: isHovered
-                ? "linear-gradient(180deg, rgba(255, 132, 25, 1) 0%, rgba(255, 173, 102, 1) 68%, rgba(255, 174, 105, 1) 100%)"
+            transition: "background 0.3s",
+            // transform: editIsHovered ? scale(1.1) : null,
+            background: editIsHovered
+                ? "linear-gradient(180deg, rgb(248, 134, 34) 0%, rgb(219, 111, 17) 32%, rgb(187, 91, 7) 100%)"
                 : "linear-gradient(180deg, rgba(255, 174, 105, 1) 0%, rgba(255, 173, 102, 1) 32%, rgba(255, 132, 25, 1) 100%)",
+        },
+        delete: {
+            padding: "10px 15px",
+            border: "none",
+            borderRadius: "5px",
+            color: "white",
+            cursor: "pointer",
+            fontSize: "16px",
+            fontWeight: "bold",
+            margin: "50px 0px 50px 5px",
+            transition: "background 0.3s",
+            // transform: editIsHovered ? scale(1.1) : null, 
+            background: "linear-gradient(180deg, rgba(255, 174, 105, 1) 0%, rgba(255, 173, 102, 1) 32%, rgba(255, 132, 25, 1) 100%)",
         }
     };
 
@@ -50,17 +76,26 @@ function Story ( {story_id, story_name, points, checkbox, onToggleCheckbox, onDe
             <div className="storyText" style={styles.storyText}>
                 <h3>User Story</h3>
                 <p> {story_name}</p>
-                <p> {points} points</p>
+                <p> {points} Points</p>
             </div>
 
             <div style={styles.clickables}>
-                <input type="checkbox" checked={checkbox}
-                 style={styles.checkbox} onChange={() => onToggleCheckbox(story_id, checkbox)}/>
-                <button style={styles.button} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-                    Edit
+                <input style={styles.checkbox} 
+                    type="checkbox"
+                    checked={checkbox}
+                    onChange={() => {
+                        setChecked(!check);
+                        onToggleCheckbox(story_id, checkbox);}}/>
+                <button style={styles.edit} 
+                    onMouseEnter={() => setEditIsHovered(true)} 
+                    onMouseLeave={() => setEditIsHovered(false)}>
+                        Edit
                 </button>
-                <button style={styles.button} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} onClick={() => onDelete(story_id) }>
-                    Delete
+                <button style={styles.delete} 
+                    onMouseEnter={() => setDeleteIsHovered(true)} 
+                    onMouseLeave={() => setDeleteIsHovered(false)} 
+                    onClick={() => onDelete(story_id) }>
+                        Delete
                 </button>                
             </div>
         </div>
